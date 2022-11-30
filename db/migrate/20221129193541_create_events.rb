9b0882caf6eae74d741e88fbc3dfcca734a46fc4
AlexGechovski <aleksg00077@gmail.com>
2022-11-30 12:31:56 +0200

    Added Events

---
+++ db/migrate/20221129193541_create_events.rb
@@ -0,0 +1,13 @@
+class CreateEvents < ActiveRecord::Migration[7.0]
+  def change
+    create_table :events do |t|
+      t.string :title
+      t.text :desciption
+      t.text :location
+      t.datetime :start_date
+      t.datetime :end_date
+
+      t.timestamps
+    end
+  end
+end
---
+++ test/system/events_test.rb
@@ -0,0 +1,49 @@
+require "application_system_test_case"
+
+class EventsTest < ApplicationSystemTestCase
+  setup do
+    @event = events(:one)
+  end
+
+  test "visiting the index" do
+    visit events_url
+    assert_selector "h1", text: "Events"
+  end
+
+  test "should create event" do
+    visit events_url
+    click_on "New event"
+
+    fill_in "Desciption", with: @event.desciption
+    fill_in "End date", with: @event.end_date
+    fill_in "Location", with: @event.location
+    fill_in "Start date", with: @event.start_date
+    fill_in "Title", with: @event.title
+    click_on "Create Event"
+
+    assert_text "Event was successfully created"
+    click_on "Back"
+  end
+
+  test "should update Event" do
+    visit event_url(@event)
+    click_on "Edit this event", match: :first
+
+    fill_in "Desciption", with: @event.desciption
+    fill_in "End date", with: @event.end_date
+    fill_in "Location", with: @event.location
+    fill_in "Start date", with: @event.start_date
+    fill_in "Title", with: @event.title
+    click_on "Update Event"
+
+    assert_text "Event was successfully updated"
+    click_on "Back"
+  end
+
+  test "should destroy Event" do
+    visit event_url(@event)
+    click_on "Destroy this event", match: :first
+
+    assert_text "Event was successfully destroyed"
+  end
+end
