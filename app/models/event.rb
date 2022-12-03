9c89c1f8d39a7dfa531e0d0ee55f52372c9f5fc7
AlexGechovski <aleksg00077@gmail.com>
2022-12-03 23:01:21 +0200

    Added more tests for Events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,6 @@
 class Event < ApplicationRecord
-    validates :title , presence: true , length: { in: 6..150 }
+    validates :title , presence: true , length: { in: 4..150 }
+    validates :location , presence: true, length: { in: 6..150 }
     validates :start_date , presence: true 
     validates :end_date , presence: true ,comparison: { greater_than_or_equal_to: :start_date }
 end
--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -1,11 +1,67 @@
 require "test_helper"
 
 class EventTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
   test "Should not save event without title" do
-    event = Event.new
-    assert_not event.save
+    event = Event.new(title: "",
+      location: "Sofia,Bulgaria",
+      start_date: "2022-12-03 22:08:00 UTC",
+      end_date: "2022-12-03 22:08:00 UTC")
+    assert_not event.valid?
+  end
+  test "Title shouldn't be less than 4 characters" do
+    event = Event.new(title: "Opa",
+      location: "Sofia,Bulgaria",
+      start_date: "2022-12-03 22:08:00 UTC",
+      end_date: "2022-12-03 22:08:00 UTC")
+    assert_not event.valid?
+  end
+  test "Title shouldn't be more than 150 characters" do
+    event = Event.new(title: "ycqzuuzutidbzchpfaacophtwpeubuzphzaeemksvwybhcvltukiscofijdouwdkddnrekaupyjrhvhrertcfvmmqbfskkpomumfhqgclaeewqlwioapkoujjhfrkooaphhpobrlkmlhqhpjpgfinlz",
+      location: "Sofia,Bulgaria",
+      start_date: "2022-12-03 22:08:00 UTC",
+      end_date: "2022-12-03 22:08:00 UTC")
+    assert_not event.valid?
+  end
+  test "Should save event with title" do
+    event = Event.new(title: "Test Event",
+      location: "Sofia,Bulgaria",
+      start_date: "2022-12-03 22:08:00 UTC",
+      end_date: "2022-12-03 22:08:00 UTC")
+    assert event.valid?
+  end
+  test "Should not save event without start date" do
+    event = Event.new(title: "Test Event",
+      location: "Sofia,Bulgaria",
+      start_date: "",
+      end_date: "2022-12-03 22:08:00 UTC")
+    assert_not event.valid?
+  end
+  test "Should not save event without end date" do
+    event = Event.new(title: "Test Event",
+      location: "Sofia,Bulgaria",
+      start_date: "2022-12-03 22:08:00 UTC",
+      end_date: "")
+    assert_not event.valid?
+  end
+  test "Start date shouldn't be after end date" do
+    event = Event.new(title: "Test Event",
+      location: "Sofia,Bulgaria",
+      start_date: "2022-12-03 22:08:00 UTC",
+      end_date: "2022-12-02 22:08:00 UTC")
+    assert_not event.valid?
+  end
+  test "Shouldn't save without location" do
+    event = Event.new(title: "Test Event",
+      location: "",
+      start_date: "2022-12-03 22:08:00 UTC",
+      end_date: "2022-12-02 22:08:00 UTC")
+    assert_not event.valid?
+  end
+  test "Location shouldn't be less than 6 characters" do
+    event = Event.new(title: "Test Event",
+      location: "Sofia",
+      start_date: "2022-12-03 22:08:00 UTC",
+      end_date: "2022-12-02 22:08:00 UTC")
+    assert_not event.valid?
   end
 end
