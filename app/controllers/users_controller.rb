15b7834fecff20828d94332ae0bd87aa4270e665
AlexGechovski <aleksg00077@gmail.com>
2023-02-12 00:26:46 +0200

    Added attending events at user show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,12 @@
 class UsersController < ApplicationController
 
-
   def index
     @users = User.all
   end
+
   def show
     @user = User.find_by_id(params[:id])
     @events = Event.where(user_id: @user.id)
+    @attended_events = Attendance.where(user_id: @user.id)
   end
 end
