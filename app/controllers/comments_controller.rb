99242f8887e0abf28f4802026dc880e45dc4b64a
AlexGechovski <aleksg00077@gmail.com>
2023-05-20 16:44:13 +0300

    Fixed comments input

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -34,7 +34,8 @@ class CommentsController < ApplicationController
         # format.html { redirect_to event_url(@comment.event_id), notice: "Comment was successfully created." }
         format.turbo_stream { render turbo_stream: turbo_stream.prepend("comments", @comment) }
       else
-        format.html { render :new, status: :unprocessable_entity }
+        #format.html { render :new, status: :unprocessable_entity }
+        format.turbo_stream { render turbo_stream: turbo_stream.replace("comment_form", partial: "comments/form", locals: { comment: @comment }) }
       end
     end
   end
